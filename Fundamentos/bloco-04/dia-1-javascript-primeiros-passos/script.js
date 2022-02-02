5d1c5a77e9014d0d4b566fdd616a257aa5dcc33a
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-02-02 19:18:24 -0300

    adiciona os itens 2, 3 e 4

--- Fundamentos/bloco-04/dia-1-javascript-primeiros-passos/script.js
+++ Fundamentos/bloco-04/dia-1-javascript-primeiros-passos/script.js
@@ -1,3 +1,5 @@
+// Item 1
+
 const a = 12;
 const b = 3;
 
@@ -15,3 +17,54 @@ let div = a / b;
 
 //Resto
 let mod = a % b;
+
+
+// Item 2
+
+const num1 = 25;
+const num2 = 30;
+
+if (num1 > num2) {
+    console.log(num1);
+}
+else if (num1 < num2) {
+    console.log(num2);
+}
+else {
+    console.log('números iguais');
+}
+
+
+// Item 3
+
+const x = 15;
+const y = 18;
+const z = 20;
+
+if (x > y && x > z) {
+    console.log(x);
+}
+else if (y > x && y > z) {
+    console.log(y);
+}
+else if (z > x && z > y) {
+    console.log(z);
+}
+else {
+    console.log('existem números iguais');
+}
+
+
+// Item 4
+
+const value = -3;
+
+if (value > 0) {
+    console.log('positve');
+}
+else if (value < 0) {
+    console.log('negative');
+}
+else {
+    console.log('zero');
+}
