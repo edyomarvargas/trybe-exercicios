5a4859c08ff83d212c4153f4582907bc21aa3226
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-02-02 19:07:28 -0300

    adiciona item 1

---
+++ Fundamentos/bloco-04/dia-1-javascript-primeiros-passos/script.js
@@ -0,0 +1,17 @@
+const a = 12;
+const b = 3;
+
+//Soma
+let soma = a + b;
+
+//Subtração
+let sub = a - b;
+
+//Multiplicação
+let mult = a * b;
+
+//Divisão
+let div = a / b;
+
+//Resto
+let mod = a % b;
