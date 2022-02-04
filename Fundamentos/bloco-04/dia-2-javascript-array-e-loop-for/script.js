62c568ea223b019478ba1bad84da2df673f651b3
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-02-04 10:46:23 -0300

    adiciona itens 1 e 2

---
+++ Fundamentos/bloco-04/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,21 @@
+// Item 1
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (i = 0; i < numbers.length; i++) {
+    // console.log(numbers[i]);
+}
+
+
+// Item 2
+
+let soma = 0;
+
+for (i = 0; i < numbers.length; i++) {
+    soma += numbers[i];
+}
+
+console.log(soma);
+
+
+// Item 3
