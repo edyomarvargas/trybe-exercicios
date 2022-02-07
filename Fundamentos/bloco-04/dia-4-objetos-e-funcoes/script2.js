7bea5142f265ba0ff5b8079c75ea1b113e8a1e16
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-02-07 13:02:04 -0300

    adiciona itens 5, 6, 7 e 8

--- Fundamentos/bloco-04/dia-4-objetos-e-funcoes/script2.js
+++ Fundamentos/bloco-04/dia-4-objetos-e-funcoes/script2.js
@@ -25,5 +25,59 @@ for (key in info) {
 // Item 4
 
 for (key in info) {
-    console.log(info[key]);
+    // console.log(info[key]);
 }
+
+// Item 5
+
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+for (key in info) {
+    if (key === 'recorrente' && info[key] === 'Sim' && info2[key] === 'Sim') {
+        // console.log('Ambos recorrentes');
+    } 
+    else {
+        // console.log(info[key] + ' e ' + info2[key]);
+    }
+}
+
+
+// Item 6
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+};
+
+// console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama ' + leitor.livrosFavoritos[0].titulo);
+
+
+// Item 7 
+
+leitor['livrosFavoritos'].push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+    }
+)
+
+// console.log(leitor.livrosFavoritos);
+
+// Item 8
+
+console.log(leitor['nome'] + ' tem ' + leitor['livrosFavoritos'].length + ' livros favoritos.');
+
