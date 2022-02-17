f79df98552bfb16ec9bcdda9d1f7ef7b9eea1edf
Edyomar Vargas <edyomar-vargas@hotmail.com>
2022-02-17 11:05:41 -0300

    adiciona requisito 1

--- fundamentos/bloco-05/dia-3-eventos/script.js
+++ fundamentos/bloco-05/dia-3-eventos/script.js
@@ -1,14 +1,51 @@
 function createDaysOfTheWeek() {
-    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
-    const weekDaysList = document.querySelector('.week-days');
-  
-    for (let index = 0; index < weekDays.length; index += 1) {
-      const days = weekDays[index];
-      const dayListItem = document.createElement('li');
-      dayListItem.innerHTML = days;
-  
-      weekDaysList.appendChild(dayListItem);
-    };
-  };
-  
-  createDaysOfTheWeek();
+	const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+	const weekDaysList = document.querySelector('.week-days');
+
+	for (let index = 0; index < weekDays.length; index += 1) {
+		const days = weekDays[index];
+		const dayListItem = document.createElement('li');
+		dayListItem.innerHTML = days;
+
+		weekDaysList.appendChild(dayListItem);
+	};
+};
+
+createDaysOfTheWeek();
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function addDays(daysList) {
+	let days = document.getElementById('days');
+	for (let index = 0; index < daysList.length; index += 1) {
+		let dayOfTheWeek = daysList[index];
+
+		if (dayOfTheWeek === 24 || dayOfTheWeek === 31) {
+			let newListItem = document.createElement('li');
+			newListItem.className = 'day holiday';
+			newListItem.innerText = dayOfTheWeek;
+			days.appendChild(newListItem);
+		}
+		else if (dayOfTheWeek === 4 || dayOfTheWeek === 11 || dayOfTheWeek === 18) {
+			let newListItem = document.createElement('li');
+			newListItem.className = 'day friday';
+			newListItem.innerText = dayOfTheWeek;
+			days.appendChild(newListItem);
+		}
+		else if (dayOfTheWeek === 25) {
+			let newListItem = document.createElement('li');
+			newListItem.className = 'day holiday friday';
+			newListItem.innerText = dayOfTheWeek;
+			days.appendChild(newListItem);
+		}
+		else {
+			let newListItem = document.createElement('li');
+			newListItem.className = 'day';
+			newListItem.innerText = dayOfTheWeek;
+			days.appendChild(newListItem);
+		}
+	}
+}
+
+addDays(dezDaysList);
+
